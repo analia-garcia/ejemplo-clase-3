bb20db7eee2e0c2aa7e117ca8e2be25d6f5202dd
Any Garcia <analia-garcia@live.com>
2022-05-04 15:55:49 -0300

    condicional en java

---
+++ condicional.java
@@ -0,0 +1,95 @@
+package condicionales;
+
+public class condicional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * int a = 10; int b = 20; if(a == b) { System.out.println("son iguales"); }
+		 * else if (a < b ) { System.out.println("son diferentes y a es menor que b"); }
+		 * else { System.out.println("son diferentes y b es menor que a");
+		 * 
+		 * }
+		 */
+		/*
+		 * String txt1 = "esta es la primer frase"; String txt2 =
+		 * "Esta es la primer frase"; if (txt1 == txt2) {
+		 * System.out.println("son iguales"); } else {
+		 * System.out.println("son diferentes"); }
+		 */
+		/*
+		 * int limInf = 10; int limSup = 50; int valor = 50;
+		 * 
+		 * if (valor >= limInf && valor <= limSup) { String msg =
+		 * "Esta dentro del rango"; if (valor == limInf) { msg +=
+		 * ", justo en el limite inferior"; } else if (valor == limSup) { msg +=
+		 * ", justo en el limite superior"; } System.out.println(msg); } else if (valor
+		 * <= limInf) { System.out.println("esta por debajo del limite inferior"); }
+		 * else {
+		 * 
+		 * System.out.println("esta por sobre el limite superior"); }
+		 */
+		// EJERCICIO
+		/*
+		 * String ruedas = "2"; String propulsion1 = "sangre";
+		 * 
+		 * 
+		 * if (ruedas == "2" & propulsion1== "sangre") {
+		 * System.out.println("bicicleta"); } else { System.out.println("moto"); } if
+		 * (ruedas == "3" & propulsion1 == "sangre") {
+		 * System.out.println("triciclo a pedal"); } else {
+		 * System.out.println("triciclo"); } if (ruedas == "4" & propulsion1 ==
+		 * "sangre") { System.out.println("karting a pedal"); } else {
+		 * System.out.println("auto, cuatriciclo o camioneta"); }if (ruedas == "5" &
+		 * propulsion1 == "sangre") { System.out.println("no existe"); } else {
+		 * System.out.println("camion"); }
+		 */
+        /*
+		Scanner inputObj = new Scanner(System.in);
+		System.out.println("Defina el tipo de propulsion (1 si es sangre 2 si es combustible:");
+		int propulsion = inputObj.nextInt();
+		System.out.println("Defina la cantidad de ruedas");
+		int ruedas = inputObj.nextInt();
+		switch (ruedas) {
+		case 2: 
+			if (propulsion == 1) {
+				System.out.println("Es una bicicleta");
+			} else {
+				System.out.println("Es una moto");
+			} 
+			break;
+		case 3:	
+			//else if(ruedas == 3) {
+			if(propulsion ==1) {
+				System.out.println("Es un triciclo a pedal");
+			} else {
+				System.out.println("Es un triciclo");
+			}
+			break;
+		case 4:	
+		//else if(ruedas == 4) {
+			if(propulsion ==1) {
+				System.out.println("Es un Karting a pedal");
+			}else {
+				System.out.println("Es un auto, cuatriciclo o camioneta");
+			} 
+			break;
+		default:
+		//}else if(ruedas >4) {
+			if(propulsion == 1) {
+				System.out.println("No existe");
+			}else {
+				System.out.println("Es un camion");
+			}
+		}
+		*/
+		
+		
+		
+		
+		
+		
+		
+	}
+}
