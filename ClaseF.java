700e8a6f141b733949a6f0eb7453212b1b9fbd84
Any Garcia <analia-garcia@live.com>
2022-05-04 16:42:04 -0300

    bucle y claseF

---
+++ ClaseF.java
@@ -0,0 +1,18 @@
+package bucles;
+
+
+public class ClaseF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	int a = 1;
+	int b = 2;
+	if (a=b) {
+		System.out.println(“A es igual a B”);
+	 }
+	}
+
+ 
